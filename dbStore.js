910bc3dea1e4ce068129d670dbf0eeb3fcfb431d
Nicu <prunku@gmail.com>
2015-08-07 16:40:47 +0300

    First version

---
+++ dbStore.js
@@ -0,0 +1,212 @@
+/**
+ * @license dbStore v0.1.0
+ * (c) 2015 Nicu Ciocan, https://github.com/nicu
+ * License: MIT
+ */
+(function(window, angular, undefined) {
+    'use strict';
+
+    angular.module('dbStore', [])
+      .factory('FixtureStore', function() {
+        var __local_store = {};
+
+        // private map of all relations between collections
+        var relations = {};
+
+        var FixtureStore = {};
+
+        FixtureStore.get = function(name) {
+          return JSON.parse(localStorage[name] || '[]');
+        };
+        FixtureStore.set = function(name, value) {
+          localStorage[name] = JSON.stringify(value);
+        };
+
+        FixtureStore.clear = function() {
+          localStorage.clear();
+        };
+
+        FixtureStore.genId = function(items) {
+          var ids = items.map(function(item) {
+            return item.id || 0;
+          });
+
+          // just in case there were no items in our collection
+          // assume we have a 'max' id of 0
+          ids.push(0);
+
+          return Math.max.apply(Math, ids) + 1;
+        };
+
+        FixtureStore.findIndex = function(filter, items) {
+          var keys = Object.keys(filter),
+            index = -1;
+
+          items.some(function(item, i) {
+            return keys.reduce(function matchItem(match, key) {
+              var isMatch = (match && (item[key] === filter[key]));
+
+              if (isMatch) {
+                index = i;
+              }
+
+              return isMatch;
+            }, true);
+          });
+
+          // no item found
+          return index;
+        };
+
+        /**
+         * NOTE: the association loading goes only 1 level deep
+         * when used inside the Fixture store, it will be called
+         * recursively for the other levels
+         */
+        FixtureStore.parseAssociations = function(includes) {
+          return includes.map(function(collectionName) {
+            var keys, name, deps = [];
+
+            if (typeof collectionName === 'string') {
+              name = collectionName;
+            }
+            else {
+              keys = Object.keys(collectionName);
+              name = keys[0];
+              deps = collectionName[keys[0]];
+            }
+
+            return {
+              name: name,
+              deps: deps
+            };
+          });
+        };
+
+        FixtureStore.loadAssociations = function(name, item, associations) {
+          var self = this;
+          angular.forEach(associations, function(assoc) {
+            var where = {},
+              bToAssoc = relations[assoc.name] ? relations[assoc.name].belongsTo : [];
+
+            angular.forEach(bToAssoc, function(bTo) {
+              var k = Object.keys(bTo);
+              if (k[0] === name) {
+                where[bTo[k[0]]] = item.id;
+              }
+            });
+
+            item[assoc.name] = self.findAll(assoc.name, where, {include: assoc.deps});
+          });
+        };
+
+        FixtureStore.create = function(name, _data) {
+          var items = this.get(name),
+            data = angular.isString(_data) ? angular.fromJson(_data) : _data;
+
+          // initialise an empty array if no data exists for the store
+          if (!items) {
+            items = [];
+          }
+
+          // assign a newly generated id for the data
+          data.id = this.genId(items);
+
+          items.push(data);
+          // persist the collection
+          this.set(name, items);
+
+          return data;
+        };
+
+        FixtureStore.update = function(name, filter, _item) {
+          var items = this.get(name),
+            index = FixtureStore.findIndex(filter, items),
+            item = angular.isString(_item) ? angular.fromJson(_item) : _item;
+
+          if (index === -1) {
+            // TODO let it crash?
+            return false;
+          }
+
+          // replace the item in the collection
+          items[index] = item;
+          // persist the updated collection
+          this.set(name, items);
+
+          return true;
+        };
+
+
+        FixtureStore.findAll = function(name, filter, options) {
+          var self = this,
+            keys = Object.keys(filter || {}),
+            opts = options || {},
+            associations = this.parseAssociations(opts.include || []);
+
+          return this.get(name).filter(function(item) {
+            self.loadAssociations(name, item, associations);
+
+            return keys.reduce(function(isMatch, key) {
+              return isMatch && (item[key] === filter[key]);
+            }, true);
+          });
+        };
+
+        FixtureStore.findOne = function(name, filter, options) {
+          var items = this.get(name),
+            index = this.findIndex(filter, items),
+            item = items[index],
+            opts = options || {},
+            associations = this.parseAssociations(opts.include || []);
+
+          this.loadAssociations(name, item, associations);
+
+          return item;
+        };
+
+        FixtureStore.remove = function(name, filter) {
+          var items = this.get(name),
+            index = this.findIndex(filter, items);
+
+          if (index > -1) {
+            // remove the item from the collection
+            items.splice(index, 1);
+            // persist the updated collection
+            this.set(name, items);
+          }
+
+          return true;
+        };
+
+        FixtureStore.belongsTo = function(name, def) {
+          relations[name] = relations[name] || {};
+          relations[name].belongsTo = def;
+        };
+
+        return FixtureStore;
+      })
+
+      .factory('dbStore', function(FixtureStore) {
+        var dbStore = function(name) {
+          var result = {};
+
+          angular.forEach(['create', 'update', 'remove', 'findOne', 'findAll'], function(method) {
+            result[method] = function() {
+              var args = Array.prototype.slice.apply(arguments);
+              return FixtureStore[method].apply(FixtureStore, [name].concat(args));
+            };
+          });
+
+          result.belongsTo = function(def) {
+            return FixtureStore.belongsTo(name, def);
+          };
+
+          return result;
+        };
+
+        dbStore.clear = FixtureStore.clear;
+
+        return dbStore;
+      });
+})(window, window.angular);
---
+++ dbStore.spec.js
@@ -0,0 +1,219 @@
+(function() {
+  'use strict';
+
+  describe('dbStore/FixtureStore', function() {
+    var FixtureStore, dbStore;
+
+    beforeEach(module('dbStore'));
+
+    beforeEach(inject(function(_FixtureStore_, _dbStore_) {
+      FixtureStore = _FixtureStore_;
+      dbStore = _dbStore_;
+    }));
+
+    afterEach(function() {
+      FixtureStore.clear();
+    });
+
+    describe('FixtureStore', function() {
+      it('should generate the first id', function() {
+        expect(FixtureStore.genId([])).toEqual(1);
+      });
+
+      it('should generate new id based on the highest existent id', function() {
+        expect(FixtureStore.genId([{id: 1}, {id: 7}, {id: 6}])).toEqual(8);
+      });
+
+      it('should return the index of an item inside a given collection', function() {
+        expect(FixtureStore.findIndex({id: 1}, [{id: 1}, {id: 7}, {id: 6}])).toEqual(0);
+      });
+
+      it('should return -1 if an item does not exist in a given collection', function() {
+        expect(FixtureStore.findIndex({id: 10}, [{id: 1}, {id: 7}, {id: 6}])).toEqual(-1);
+      });
+
+      it('should return the associations and their dependencies', function() {
+        var assoc = FixtureStore.parseAssociations(['first', {'second': ['third', {'fourth': ['fifth', 'sixth']}]}]);
+
+        expect(assoc.length).toBe(2);
+
+        // first dependency doesn't have any other nested dependencies
+        expect(assoc[0].name).toEqual('first');
+        expect(assoc[0].deps.length).toBe(0);
+
+        // second dependency has 2 nested dependencies: `third` and `fourth`
+        expect(assoc[1].name).toEqual('second');
+        expect(assoc[1].deps.length).toBe(2);
+      });
+    });
+
+
+    describe('dbStore', function() {
+      it('should create a new item', function() {
+        var item = dbStore('collection').create({
+          name: 'New Item'
+        });
+
+        expect(item).toEqual({
+          id: 1,
+          name: 'New Item'
+        });
+      });
+
+      describe('Existing store data', function() {
+        beforeEach(function() {
+          FixtureStore.set('collection', [{
+            id: 1,
+            name: 'First item',
+            group: 'A'
+          }, {
+            id: 2,
+            name: 'Second item',
+            group: 'B'
+          }, {
+            id: 3,
+            name: 'Third item',
+            group: 'A'
+          }]);
+        });
+
+        it('should update an existing item', function() {
+          var success = dbStore('collection').update({name: 'Second item'}, {
+            id: 2,
+            name: 'Updated'
+          });
+          expect(success).toBe(true);
+
+          var item = dbStore('collection').findOne({id: 2});
+          expect(item.name).toEqual('Updated');
+        });
+
+        it('should return false when item can\'t be updated', function() {
+          var success = dbStore('collection').update({name: 'Inexistent'}, {
+            name: 'This will never be updated'
+          });
+
+          expect(success).toBe(false);
+        });
+
+        it('should remove an item', function() {
+          var items;
+
+          // before
+          items = dbStore('collection').findAll();
+          expect(items.length).toBe(3);
+
+          dbStore('collection').remove({id: 1});
+
+          // after
+          items = dbStore('collection').findAll();
+          expect(items.length).toBe(2);
+          expect(items[0].name).toEqual('Second item');
+          expect(items[1].name).toEqual('Third item');
+        });
+
+        it('should return a specific item', function() {
+          var item = dbStore('collection').findOne({id: 3});
+
+          expect(item.name).toEqual('Third item');
+        });
+
+        it('should return undefined when item does not exist', function() {
+          var item = dbStore('collection').findOne({id: 10});
+
+          expect(item).not.toBeDefined();
+        });
+
+        it('should return all items', function() {
+          var items = dbStore('collection').findAll();
+
+          expect(items.length).toBe(3);
+        });
+
+        it('should return items that match criteria', function() {
+          var groupA = dbStore('collection').findAll({group: 'A'});
+          var groupB = dbStore('collection').findAll({group: 'B'});
+
+          expect(groupA.length).toBe(2);
+          expect(groupB.length).toBe(1);
+        });
+
+        describe('Nested resources', function() {
+          beforeEach(function() {
+            FixtureStore.set('nested', [{
+              id: 1,
+              collection_id: 1,
+              name: 'First subitem'
+            }, {
+              id: 2,
+              collection_id: 1,
+              name: 'Second subitem'
+            }, {
+              id: 3,
+              collection_id: 2,
+              name: 'Third subitem'
+            }]);
+
+            FixtureStore.set('deeplyNested', [{
+              id: 1,
+              parent_id: 1,
+              name: 'First deep subitem'
+            }, {
+              id: 2,
+              parent_id: 1,
+              name: 'Second deep subitem'
+            }, {
+              id: 3,
+              parent_id: 2,
+              name: 'Third deep subitem'
+            }]);
+          });
+
+          describe('without associations', function() {
+            it('should return all items containing nested resources', function() {
+              var items = dbStore('collection').findAll({}, {include: [{'nested': ['deeplyNested']}]}),
+                item = items[0];
+
+              expect(items.length).toBe(3);
+              expect(item.nested.length).toBe(3);
+              expect(item.nested[0].deeplyNested.length).toBe(3);
+            });
+
+            it('should return one item containing nested resources', function() {
+              var item = dbStore('collection').findOne({id: 1}, {include: [{'nested': ['deeplyNested']}]});
+
+              expect(item.nested.length).toBe(3);
+              expect(item.nested[0].deeplyNested.length).toBe(3);
+            });
+          });
+
+          describe('with associations', function() {
+            beforeEach(function() {
+              dbStore('nested').belongsTo([{'collection': 'collection_id'}]);
+              dbStore('deeplyNested').belongsTo([{'nested': 'parent_id'}]);
+            });
+
+            it('should return all items containing nested resources', function() {
+              var items = dbStore('collection').findAll({}, {include: [{'nested': ['deeplyNested']}]});
+              expect(items.length).toBe(3);
+
+              expect(items[0].nested.length).toBe(2);
+              expect(items[1].nested.length).toBe(1);
+              expect(items[2].nested.length).toBe(0);
+
+              expect(items[0].nested[0].deeplyNested.length).toBe(2);
+              expect(items[0].nested[1].deeplyNested.length).toBe(1);
+            });
+
+            it('should return one item containing nested resources', function() {
+              var item = dbStore('collection').findOne({id: 1}, {include: [{'nested': ['deeplyNested']}]});
+
+              expect(item.nested.length).toBe(2);
+              expect(item.nested[0].deeplyNested.length).toBe(2);
+            });
+          });
+        });
+      });
+    });
+  });
+})();
